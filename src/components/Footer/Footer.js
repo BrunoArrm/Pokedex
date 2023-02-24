92a16aed7542e4e6c58efda03bec89dd8e8a7589
BrunoArrm <bruno.arrm@gmail.com>
2023-02-24 18:56:01 -0300

    Rodapé alterado com modificações nos direitos autorais

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,14 +6,16 @@ function Footer() {
   return (
     <footer className='footer'>
         <Pagination />
-        <div className='sign'>
-            <p>Desenvolvido orgulhosamente por Bruno Arruda</p>
+        <div className='socialMedias'>
             <a href="https://github.com/BrunoArrm/pokedex" target="_blank">
                 <div className='socialMedia github'></div>
             </a>
             <a href="https://linkedin.com/in/bruno-arruda-dev" target="_blank">
                 <div className='socialMedia linkedin'></div>
             </a>
+        </div>
+        <div className='sign'>
+            <p>Copyright © 2023 | <span>Bruno Arruda</span> | POKÉAPI | Todos os direitos reservados.</p>
         </div>
     </footer>
   )
